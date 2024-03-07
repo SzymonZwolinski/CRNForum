2fc6788b316ab14720b32eb754afb280273200ec
Szymon <szymzwoo@gmail.com>
2024-03-07 22:41:01 +0100

    Added MudBlazor, test how to change default blazor to mud

---
+++ front/Platender.Front/Pages/MainPage.razor.cs
@@ -0,0 +1,17 @@
+﻿
+using MudBlazor;
+
+namespace Platender.Front.Pages
+{
+	public partial class MainPage
+	{
+		private List<string> BestDrivers = new();
+		private MudCarousel<string> _carousel;
+		private IList<string> _source = new List<string>() { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+		protected override void OnInitialized()
+		{
+			BestDrivers.Add("ADAM MAŁYSZ");
+			BestDrivers.Add("ROBERT KUBICA");
+		}
+	}
+}
