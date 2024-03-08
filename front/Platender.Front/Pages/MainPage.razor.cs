00ac7bcda419e17af12524a2ef5acb03883d99b0
Szymon <szymzwoo@gmail.com>
2024-03-08 21:46:15 +0100

    Created reusable components, added css isolation

--- front/Platender.Front/Pages/MainPage.razor.cs
+++ front/Platender.Front/Pages/MainPage.razor.cs
@@ -6,7 +6,6 @@ namespace Platender.Front.Pages
 	public partial class MainPage
 	{
 		private List<string> BestDrivers = new();
-		private MudCarousel<string> _carousel;
 		private IList<string> _source = new List<string>() { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
 		protected override void OnInitialized()
 		{
