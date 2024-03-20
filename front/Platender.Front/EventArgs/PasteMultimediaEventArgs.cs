c7007d791e417dfcc30a1c527055f74cf62990a0
Szymon <szymzwoo@gmail.com>
2024-03-20 22:10:48 +0100

    Paste multimedia event

---
+++ front/Platender.Front/EventArgs/PasteMultimediaEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Platender.Front.Event
+{
+    public class PasteMultimediaEventArgs : EventArgs
+    {
+        public bool IsMultimedia { get; set; }
+        public string Data { get; set; }
+    }
+
+    [EventHandler("onpastemultimedia", typeof(PasteMultimediaEventArgs),
+    enableStopPropagation: true, enablePreventDefault: true)]
+    public static class EventHandlers
+    {
+
+    }
+}
---
+++ front/Platender.Front/wwwroot/PasteMultimedia.js
@@ -0,0 +1,34 @@
+﻿Blazor.registerCustomEventType("pastemultimedia", {
+    browserEventName: 'paste',
+    createEventArgs: event => {
+        let isMultimedia = false;
+
+        let data = event.clipboardData.getData('text');
+
+        const items = event.clipboardData.items;
+
+        const acceptedMediaTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+
+        for (let i = 0; i < items.length; i++) {
+            const file = items[i].getAsFile();
+
+            if (!file) {
+                continue;
+            }
+
+            if (acceptedMediaTypes.indexOf(items[i].type) === -1) {
+                continue;
+            }
+
+            isMultimedia = true;
+            const url = window.URL || window.webkitURL;
+            data = url.createObjectURL(file);
+            console.log(data.)
+        }
+
+        return {
+            isMultimedia,
+            data
+        }
+    }
+})
