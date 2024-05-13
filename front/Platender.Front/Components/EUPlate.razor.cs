2289cefdb428f8fe6dd5a960f0a2b7bc769caf39
Szymon <szymzwoo@gmail.com>
2024-05-13 16:09:55 +0200

    EUPlate improvements to be more generic, first steps to merge Login and Register into one Page

--- front/Platender.Front/Components/EUPlate.razor.cs
+++ front/Platender.Front/Components/EUPlate.razor.cs
@@ -12,6 +12,12 @@ namespace Platender.Front.Components
 
 		[Parameter]
 		public Action<Guid> OnClickAction { get; set; }
+		[Parameter]
+		public bool DisplayFlag { get; set; }
+		[Parameter]
+		public string FlagSrc { get; set; }
+		[Parameter]
+		public string BackgroundStyle { get; set; }
 
 		[Parameter]
 		public Guid PlateId { get; set; }
